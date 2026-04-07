42668bcf728df27471908de53adb763a49d6c1dd
Rodrigo Augusto Carlos Bispo <rodrigoaugustocb@gmail.com>
2026-04-06 23:57:04 -0300

    feat: implementa session guard

---
+++ src/auth/guard/session.guard.ts
@@ -0,0 +1,38 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { AuthService } from '../service/auth.service';
+
+@Injectable()
+export class SessionGuard implements CanActivate {
+  constructor(private readonly authService: AuthService) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const sessionToken = request.headers('x-session-token');
+
+    if (!sessionToken) {
+      throw new UnauthorizedException('Session token required');
+      return false;
+    }
+
+    try {
+      const session = await this.authService.validateSessionToken(sessionToken);
+      if (!session.valid || !session.user) {
+        throw new UnauthorizedException('Invalid session token');
+      }
+      request.user = session.user;
+      return true;
+    } catch (error) {
+      throw new UnauthorizedException('Invalid session token');
+    }
+  }
+}
